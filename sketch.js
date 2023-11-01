e290f560240f320de1a4a220a036b14f5c4c071c
ANDY MILLS <SM21BM@FSU.EDU>
2023-11-01 09:40:14 -0400

    checkpoint

--- sketch.js
+++ sketch.js
@@ -1,37 +1,35 @@
 let screenHeight = 737;
 let screenWidth = 1535;
-
-let enterDerailment = ['DERAILMENT', 'WHEATFIELD', 'LAMB']
-
-let index1 = 0;
+let clickCount;
 
 let masterY = 100
 
 function setup() {
   noCanvas();
 
-  //column 1
  
   backdrop = createImg('clouds.jpg');
   backdrop.size(screenWidth,screenHeight);
 
+
   let button = createImg('black button.png');
   button.size(87, 87);
   button.position(screenWidth/2,screenHeight-200);
-  button.mouseClicked(master);
- 
 
+
+  button.mouseClicked(master);
 }
 
 function master() {
-  console.log(index1);
-  let masterDisplay = createP(enterDerailment[index1]);
+
+  let masterDisplay = createP("WHEATFIELD");
   masterDisplay.position(displayWidth/2,masterY);
-  index1++;
-  console.log("Master functional!!");
+  console.log("button clicked!!");
+
+  //SPACING BETWEEN TEXT
   masterY += 45;
 }
 
-// function draw() {
-//   background(125);
-// }
+
+//make click number variable, based on mose clicks
+//switch -- p5 reference?
