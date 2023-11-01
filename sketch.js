3aeab71eea71059e1b01519e0db008567f7ff007
ANDY MILLS <SM21BM@FSU.EDU>
2023-11-01 10:28:19 -0400

    button up and running with click  counter :-)

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let screenHeight = 737;
 let screenWidth = 1535;
 let clickCount = 0;
+let masterDisplay1;
+let masterDisplay2;
 
 let masterY = 100
 
@@ -24,17 +26,21 @@ function master (){
   // clickCount = clickCount +1;
 if (clickCount<=1) {
 
-  let masterDisplay = createP("WHEATFIELD");
-  masterDisplay.position(displayWidth/2,masterY);
+  masterDisplay1 = createP("WHEATFIELD");
+  masterDisplay1.position(displayWidth/2,masterY);
   console.log("button clicked once!");
 
   //SPACING BETWEEN TEXT
   masterY += 45;
 } else {
+  masterDisplay1.hide();
+  let masterDisplay2 = createP("LAMB")
+  masterDisplay2.position(displayWidth/2,masterY);
   console.log("button clicked twice!");
 }
 }
 
+//clickcounter
 function mouseReleased () {
   clickCount++;
   }
