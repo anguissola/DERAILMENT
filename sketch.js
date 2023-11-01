cec1586c99e73c58cca3266315e5ec685c928d8f
ANDY MILLS <SM21BM@FSU.EDU>
2023-11-01 11:30:23 -0400

    on track - again - positioning needs adj

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 let screenHeight = 737;
 let screenWidth = 1535;
 let clickCount = 0;
-let masterDisplay1;
-let masterDisplay2;
+let masterDisplay1, masterDisplay2, masterDisplay3, masterDisplay4, masterDisplay5, masterDisplay6, masterDisplay7, masterDisplay8, masterDisplay9, masterDisplay10;
 
-let masterY = 100
+let masterX = 740;
+let masterY = 220
 
 function setup() {
   noCanvas();
-
  
   backdrop = createImg('clouds.jpg');
   backdrop.size(screenWidth,screenHeight);
@@ -27,22 +26,71 @@ function master (){
 if (clickCount<=1) {
 
   masterDisplay1 = createP("WHEATFIELD");
-  masterDisplay1.position(displayWidth/2,masterY);
-  console.log("button clicked once!");
+  masterDisplay1.position(masterX,masterY);
+  console.log("click 1!");
 
-  //SPACING BETWEEN TEXT
-  masterY += 45;
-} else {
+} else if (clickCount==2) {
   masterDisplay1.hide();
-  let masterDisplay2 = createP("LAMB")
-  masterDisplay2.position(displayWidth/2,masterY);
-  console.log("button clicked twice!");
+  masterDisplay2 = createP("LAMB");
+  masterDisplay2.position(masterX,masterY);
+  console.log("click 2!");
 }
+
+else if (clickCount==3) {
+  masterDisplay2.hide();
+  masterDisplay3 = createP("BUTTER");
+  masterDisplay3.position(masterX,masterY);
+  console.log("click 3!");
+}
+
+else if (clickCount==4) {
+  masterDisplay3.hide();
+  masterDisplay4 = createP("41.19692905247007, -87.04524240522085");
+  masterDisplay4.position(masterX,masterY);
+  console.log("click 4!");
+}
+
+else if (clickCount==5) {
+  masterDisplay4.hide();
+  masterDisplay5 = createImg('lambfeet.jpg');
+  masterDisplay5.position(masterX,masterY);
+  console.log("click 5!");
+}
+
+else if (clickCount==6) {
+  masterDisplay5.hide();
+  masterDisplay6 = createP("BLESSED");
+  masterDisplay6.position(masterX,masterY);
+  console.log("click 6!");
+}
+
+else if (clickCount==7) {
+  masterDisplay6.hide();
+  masterDisplay7 = createP("BE");
+  masterDisplay7.position(masterX,masterY);
+  console.log("click 7");
+}
+
+else if (clickCount==8) {
+  masterDisplay7.hide();
+  masterDisplay8 = createP("THE");
+  masterDisplay8.position(masterX,masterY);
+  console.log("click 8!");
+}
+
+else if (clickCount==9) {
+  masterDisplay8.hide();
+  masterDisplay9 = createP("NAME");
+  masterDisplay9.position(masterX,masterY);
+  console.log("click 9!");
+}
+
 }
 
 //clickcounter
 function mouseReleased () {
   clickCount++;
+  console.log("clicked");
   }
 
 
