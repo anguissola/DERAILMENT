a3b3987168d922c18f7958d82b640d0cd55a9e23
ANDY MILLS <SM21BM@FSU.EDU>
2023-11-06 09:10:04 -0500

    before array

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let screenHeight = 737;
 let screenWidth = 1535;
 let clickCount = 0;
-let masterDisplay1, masterDisplay2, masterDisplay3, masterDisplay4, masterDisplay5, masterDisplay6, masterDisplay7, masterDisplay8, masterDisplay9, masterDisplay10;
+let page1, page2, page3, page4, page5, page6, page7, page8, page9, page10;
 
 let masterX = 740;
-let masterY = 220
+let masterY = 220;
 
 function setup() {
   noCanvas();
@@ -25,63 +25,63 @@ function master (){
   // clickCount = clickCount +1;
 if (clickCount<=1) {
 
-  masterDisplay1 = createP("WHEATFIELD");
-  masterDisplay1.position(masterX,masterY);
+  page1 = createP("WHEATFIELD");
+  page1.position(masterX,masterY);
   console.log("click 1!");
 
 } else if (clickCount==2) {
-  masterDisplay1.hide();
-  masterDisplay2 = createP("LAMB");
-  masterDisplay2.position(masterX,masterY);
+  page1.hide();
+  page2 = createP("LAMB");
+  page2.position(masterX,masterY);
   console.log("click 2!");
 }
 
 else if (clickCount==3) {
-  masterDisplay2.hide();
-  masterDisplay3 = createP("BUTTER");
-  masterDisplay3.position(masterX,masterY);
+  page2.hide();
+  page3 = createP("BUTTER");
+  page3.position(masterX,masterY);
   console.log("click 3!");
 }
 
 else if (clickCount==4) {
-  masterDisplay3.hide();
-  masterDisplay4 = createP("41.19692905247007, -87.04524240522085");
-  masterDisplay4.position(masterX,masterY);
+  page3.hide();
+  page4 = createP("41.19692905247007, -87.04524240522085");
+  page4.position(masterX,masterY);
   console.log("click 4!");
 }
 
 else if (clickCount==5) {
-  masterDisplay4.hide();
-  masterDisplay5 = createImg('lambfeet.jpg');
-  masterDisplay5.position(masterX,masterY);
+  page4.hide();
+  page5 = createImg('lambfeet.jpg');
+  page5.position(masterX,masterY);
   console.log("click 5!");
 }
 
 else if (clickCount==6) {
-  masterDisplay5.hide();
-  masterDisplay6 = createP("BLESSED");
-  masterDisplay6.position(masterX,masterY);
+  page5.hide();
+  page6 = createP("BLESSED");
+  page6.position(masterX,masterY);
   console.log("click 6!");
 }
 
 else if (clickCount==7) {
-  masterDisplay6.hide();
-  masterDisplay7 = createP("BE");
-  masterDisplay7.position(masterX,masterY);
+  page6.hide();
+  page7 = createP("BE");
+  page7.position(masterX,masterY);
   console.log("click 7");
 }
 
 else if (clickCount==8) {
-  masterDisplay7.hide();
-  masterDisplay8 = createP("THE");
-  masterDisplay8.position(masterX,masterY);
+  page7.hide();
+  page8 = createP("THE");
+  page8.position(masterX,masterY);
   console.log("click 8!");
 }
 
 else if (clickCount==9) {
-  masterDisplay8.hide();
-  masterDisplay9 = createP("NAME");
-  masterDisplay9.position(masterX,masterY);
+  page8.hide();
+  page9 = createP("NAME");
+  page9.position(masterX,masterY);
   console.log("click 9!");
 }
 
